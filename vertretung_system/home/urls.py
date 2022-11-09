8a7ffb0ae3308f1fcf52c153aa4c0d5c7c6acbd4
Adib <at@onefid.com>
2022-11-09 16:19:06 +0100

    add the POST GET to the antrag endpoint

--- vertretung_system/home/urls.py
+++ vertretung_system/home/urls.py
@@ -7,12 +7,13 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'users', UserAPI)
 router.register(r'usertype', UserTypeAPI)
-router.register(r'antrag', AntragAPI)
+router.register(r'allantrags', AntragAPI)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('antrag/', views.antrag, name='antrag'),
     path('', include(router.urls)),
     
 ]
--- vertretung_system/home/views.py
+++ vertretung_system/home/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from .models import *
 from rest_framework import viewsets
 from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework import status
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -12,8 +16,27 @@ def login(request):
 def logout(request):
     return HttpResponse("hier insert loggout.html")
 
-def formular(reques):
-    return HttpResponse("hier insert formular.html")
+@api_view(['GET', 'POST', 'DELETE'])
+def antrag(request):
+    if request.method == 'GET':
+        antrags = Antrag.objects.all()
+        
+        creator_user = request.query_params.get('creator_user', None)
+        if creator_user is not None:
+            antrags = antrags
+        
+        antrags_serializer = AntragSerializer(antrags, many=True)
+        return JsonResponse(antrags_serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        antrag_data = JSONParser().parse(request)
+        antrags_serializer = AntragSerializer(data=antrag_data)
+        if antrags_serializer.is_valid():
+            antrags_serializer.save()
+            return JsonResponse(antrags_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(antrags_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    
 
 class UserTypeAPI(viewsets.ModelViewSet):
     queryset = UserType.objects.all()
